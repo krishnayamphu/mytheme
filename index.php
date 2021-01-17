029f3015160c60d36a237e4c82b903b89da406db
KrishnaYamphu <krishnayamphu@gmail.com>
2021-01-17 17:01:14 +0545

    homepage

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php get_header(); ?>
 
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-6">
-                <ul class="list-group">
-                    <?php
-                    if (have_posts()) :
-                    while (have_posts()) : the_post();
-                        ?>
-                        <li class="list-group-item">
-                            <a href="<?php the_permalink() ?>">
+<div class="container py-5">
+    <div class="row">
+        <div class="col-9">
+            <?php $catquery = new WP_Query('category=3&posts_per_page=1&offset=0'); ?>
+            <?php while ($catquery->have_posts()) : $catquery->the_post(); ?>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h1><a href="<?php the_permalink() ?>">
                                 <?php the_title(); ?>
                             </a>
+                        </h1>
 
-                            <p><?php echo excerpt(30, "..."); ?></p>
-                        </li>
-                    <?php endwhile; ?>
-                </ul>
-                <?php endif; ?>
-            </div>
+                        <?php the_post_thumbnail('full', array('class' => 'card-img-top post-img'));
+                        ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        </div>
 
-            <div class="col-6">
-                <?php if ( is_active_sidebar( 'recent-post' ) ) : ?>
-                <?php dynamic_sidebar( 'recent-post' ); ?>
-                <?php endif; ?>
-            </div>      
+        <div class="col-3">
+            <?php if (is_active_sidebar('recent-post')) : ?>
+                <?php dynamic_sidebar('recent-post'); ?>
+            <?php endif; ?>
         </div>
     </div>
+</div>
 
 <?php get_footer(); ?>
