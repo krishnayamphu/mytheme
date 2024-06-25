532c060fa274785164b4ea1d062efc60e0c6cf6d
KrishnaYamphu <krishnayamphu@gmail.com>
2024-06-25 17:52:28 +0545

    theme

---
+++ home.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="bg-info">
+        <h3 class="text-primary text-center"><?php bloginfo("name") ?></h3>
+        <p class="text-center"><?php bloginfo("description") ?></p>
+    </header>
+
+    <main>
+        <?php while (have_posts()) : the_post(); ?>
+            <h2><a href="<?php the_permalink() ?>">
+                    <?php the_title(); ?>
+                </a></h2>
+        <?php endwhile; ?>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ single.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+
+<main>
+    <div class="container">
+        <?php while (have_posts()) : the_post(); ?>
+            <h1><?php the_title(); ?></h1>
+            <?php the_content() ?>
+        <?php endwhile; ?>
+    </div>
+</main>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
